e55117fcc09b8e1b058926a7c62274bfd92fee38
Gen705 <your>
2017-07-12 11:07:43 +0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get root" do
     get root_path
-    assert_response success
+    assert_response :success
   end
 
   test "should get home" do
